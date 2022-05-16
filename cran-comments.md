db9fa379ee2023f76b56834df895cd748b355cff
Luis Alvarez <lalvarez.mat@gmail.com>
2022-05-16 15:26:21 +0100

    Commit 5

--- cran-comments.md
+++ cran-comments.md
@@ -1,5 +1,11 @@
-## First submission of the EpiInvert package
+## Resubmission
+This is a resubmission. In this version I have, following the suggestions of 
+Uwe Ligges (CRAN Teams):
 
+* Reduced each example to less than 5 sec. 
+
+I have checked again the package : 
+  
 ### R CMD check results
 * 0 errors ✔ | 0 warnings ✔ | 0 notes ✔
 
--- tests/testthat/test-USA_parametric_si.R
+++ tests/testthat/test-USA_parametric_si.R
@@ -3,38 +3,59 @@ test_that("EpiInvert using USA data", {
   data("festives")
   data("si_distr_data")
   
-  # test using parametric serial interval 
-  res <- EpiInvert(incidence$USA,incidence$date[length(incidence$date)],
-                   festives$USA)
-  x <- round(res$power_a+0.003,digits=2)
-  expect_equal(x, 1.1)
+  # test using parametric serial interval and the last 70 incidence values
+  res <- EpiInvert(incidence$USA,
+                   incidence$date[length(incidence$date)],
+                   festives$USA,
+                   EpiInvert::select_params(list(max_time_interval = 70))
+                   )
+  x <- round(res$power_a+0.0018224,digits=2)
+  expect_equal(x,-0.31)
   
-  # test using non parametric serial interval 
-  res <- EpiInvert(incidence$USA,incidence$date[length(incidence$date)],festives$USA,EpiInvert::select_params(list(si_distr = si_distr_data,shift_si_distr=1)))
-  x <- round(res$power_a+0.004,digits=2)
-  expect_equal(x, 1.09)
+  # test using non parametric serial interval and the last 70 incidence values
+  res <- EpiInvert(incidence$USA,
+                   incidence$date[length(incidence$date)],
+                   festives$USA,
+                   EpiInvert::select_params(list(si_distr = si_distr_data,shift_si_distr=1,max_time_interval = 70))
+                   )
+  x <- round(res$power_a+0.00809614,digits=2)
+  expect_equal(x, -0.22)
   
-  # test without using festive days
-  res <- EpiInvert(incidence$USA,incidence$date[length(incidence$date)])
-  x <- round(res$power_a+0.007,digits=2)
-  expect_equal(x, 1.07)
+  # test without using festive days and the last 70 incidence values
+  res <- EpiInvert(incidence$USA,
+                   incidence$date[length(incidence$date)],
+                   "1000-01-01",
+                   EpiInvert::select_params(list(max_time_interval = 70))
+                   )
+  x <- round(res$power_a+0.0018224,digits=2)
+  expect_equal(x, -0.31)
   
   # test using a different time interval 
-  res <-  EpiInvert(incidence$USA,incidence$date[length(incidence$date)],festives$USA, EpiInvert::select_params(list(max_time_interval = 100)))
-  x <- round(res$power_a-0.005,digits=2)
-  expect_equal(x, 0.59)
+  res <-  EpiInvert(incidence$USA,
+                    incidence$date[length(incidence$date)],
+                    festives$USA, 
+                    
+                    EpiInvert::select_params(list(max_time_interval = 80)))
+  x <- round(res$power_a+0.0060459,digits=2)
+  expect_equal(x, -0.43)
   
-  # test using a different parametric serial interval 
-  res <-  EpiInvert(incidence$USA,incidence$date[length(incidence$date)],festives$USA, EpiInvert::select_params(list( mean_si = 11,sd_si=7,shift_si=-2)))
-  x <- round(res$power_a-0.003,digits=2)
-  expect_equal(x,1.03)
+  # test using a different parametric serial interval and the last 70 incidence values
+  res <-  EpiInvert(incidence$USA,
+                    incidence$date[length(incidence$date)],
+                    festives$USA, 
+                    EpiInvert::select_params(list( mean_si = 11,sd_si=7,shift_si=-2,max_time_interval = 70))
+                    )
+  x <- round(res$power_a+0.0022343,digits=2)
+  expect_equal(x,-0.19)
   
-  # test using different regularization values
-  res <-  EpiInvert(incidence$USA,incidence$date[length(incidence$date)],festives$USA, EpiInvert::select_params(list( Rt_regularization_weight=10,seasonality_regularization_weight=20)))
-  x <- round(res$power_a-0.003,digits=2)
-  expect_equal(x,1.05)
+  # test using different regularization values and the last 70 incidence values
+  res <-  EpiInvert(incidence$USA,
+                    incidence$date[length(incidence$date)],
+                    festives$USA, 
+                    EpiInvert::select_params(list( Rt_regularization_weight=10,seasonality_regularization_weight=20,max_time_interval = 70))
+                    )
+  x <- round(res$power_a+0.0022683,digits=2)
+  expect_equal(x,-0.32)
   
-  
-
   
 })
